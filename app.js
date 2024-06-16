622df32e4a883884ec4d30ab5137e688d5349a3f
Sorelys Sandoval <sorboy11@gmail.com>
2024-06-16 12:27:27 -0500

    checkpoint

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ dragElement(document.getElementById("magnet"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+
+  // otherwise, move the DIV from anywhere inside the DIV:
+  elmnt.onmousedown = dragMouseDown;
+
 
   function dragMouseDown(e) {
     e = e || window.event;
